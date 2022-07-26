e5a470dcdc9c67ceabd79a142ea68c5d1ec2fd96
Anssi Piirainen <api@iki.fi>
2022-07-26 06:41:35 +0200

    Simplify syntax with if let

--- src/online_vault.rs
+++ src/online_vault.rs
@@ -13,22 +13,16 @@ pub async fn grep(access_token: &str, master_password: &str, grep: &str) -> Vec<
     };
     let result = &mut Vec::new();
     for vault in vaults {
-        match vault.credentials {
-            Some(credentials) => {
-                for creds in credentials {
-                    match creds {
-                        // TODO: convert to the other Credentials type
-                        // OR switch completely to the graphql type !
-                        Some(cred) => result.push(CredentialsModel {
-                            password: cred.password,
-                            username: cred.username,
-                            service: cred.service,
-                        }),
-                        _ => (),
-                    }
+        if let Some(credentials) = vault.credentials {
+            for creds in credentials {
+                if let Some(cred) = creds {
+                    result.push(CredentialsModel {
+                        password: cred.password,
+                        username: cred.username,
+                        service: cred.service,
+                    })
                 }
             }
-            _ => (),
         }
     }
     result.to_vec()
