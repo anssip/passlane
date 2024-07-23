1bd832b812aae2bd13d069a0c8a572037d36e6f3
Anssi Piirainen <api@iki.fi>
2024-07-23 16:31:46 +0300

    chore: clean up CSV handling

--- src/actions/import.rs
+++ src/actions/import.rs
@@ -1,8 +1,8 @@
-use clap::ArgMatches;
 use crate::actions::UnlockingAction;
 use crate::store;
 use crate::vault::entities::Error;
 use crate::vault::vault_trait::Vault;
+use clap::ArgMatches;
 
 pub struct ImportCsvAction {
     pub file_path: String,
@@ -11,15 +11,16 @@ pub struct ImportCsvAction {
 impl ImportCsvAction {
     pub fn new(matches: &ArgMatches) -> ImportCsvAction {
         ImportCsvAction {
-            file_path: matches.get_one::<String>("FILE_PATH").expect("required").to_string(),
+            file_path: matches
+                .get_one::<String>("FILE_PATH")
+                .expect("required")
+                .to_string(),
         }
     }
 }
 
 fn push_from_csv(vault: &mut Box<dyn Vault>, file_path: &str) -> Result<i64, Error> {
-    let input = store::read_from_csv(file_path)?;
-    let creds = input.into_iter().map(|c| c.to_credential()).collect();
-
+    let creds = store::read_from_csv(file_path)?;
     vault.save_credentials(&creds)?;
     let num_imported = creds.len();
     Ok(num_imported.try_into().unwrap())
@@ -28,6 +29,7 @@ fn push_from_csv(vault: &mut Box<dyn Vault>, file_path: &str) -> Result<i64, Err
 impl UnlockingAction for ImportCsvAction {
     fn run_with_vault(&self, vault: &mut Box<dyn Vault>) -> Result<Option<String>, Error> {
         push_from_csv(vault, &self.file_path)
-            .map(|count| format!("Imported {} entries", count)).map(Some)
+            .map(|count| format!("Imported {} entries", count))
+            .map(Some)
     }
 }
